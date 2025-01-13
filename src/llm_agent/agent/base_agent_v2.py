ea94455cdf9fe791e957b102ddb3a092d26376c0
VSAnimator <sarukkai@stanford.edu>
2025-01-13 11:15:08 -0800

    Organized base_agent by sections

--- src/llm_agent/agent/base_agent_v2.py
+++ src/llm_agent/agent/base_agent_v2.py
@@ -45,22 +45,9 @@ class BaseAgent:
         # Database
         self.db = db
 
-    def get_in_context_data(self, key_type, key, value_type, outcome="winning", k=5, window=20) -> List[Dict]:
-        """Retrieve in context examples from the database"""
-        success_entries, failure_entries = self.db.get_similar_entries(key_type, key, outcome=outcome, k=k, window=window)
-        # Now figure out which part of the examples to return in-context
-        if not isinstance(value_type, list): # Check that this is a list, not a string
-            value_type = [value_type]
-        # Filter out the keys that are not in the value_type
-        success_entries = [{k: v for k, v in entry.items() if k in value_type} for entry in success_entries]
-        failure_entries = [{k: v for k, v in entry.items() if k in value_type} for entry in failure_entries]
-        return success_entries, failure_entries
-    
-    def store_episode(self, reflection, summary):
-        """Store an episode in the database"""
-        self.db.store_episode(self.environment_id, self.goal, self.category,self.observation_history, self.reasoning_history, self.action_history, self.reward_history, self.plan, reflection, summary)
+    """ Helper functions """
 
-    def create_conversation(self, keys: List[str], available_actions: List[Action]) -> List[Dict]:
+    def _create_conversation(self, keys: List[str], available_actions: List[Action]) -> List[Dict]:
         """Create a conversation with the observation and action history"""
         # First create a dictionary of the current trajectory. Valid keys are "plan", "observation", "reasoning", "action"
         trajectory_dict = {}
@@ -81,14 +68,14 @@ class BaseAgent:
             elif key == "available_actions":
                 trajectory_dict[key] = repr([f"{i+1}. {a.text}" for i, a in enumerate(available_actions)])
         # Create a string of the current trajectory by rolling up the trajectory_dict
-        trajectory_dict = self.roll_up_trajectory([trajectory_dict])[0]
+        trajectory_dict = self._roll_up_trajectory([trajectory_dict])[0]
         # Create a string of the current trajectory
         trajectory_string = "Current trajectory:\n"
         for key in trajectory_dict:
             trajectory_string += f"{key}: {trajectory_dict[key]}\n"
         return trajectory_string
     
-    def roll_up_trajectory(self, entries):
+    def _roll_up_trajectory(self, entries):
         interleaved_keys = ["observation", "reasoning", "action"]
         """Roll up the trajectory into a single string"""
         value_type = list(entries[0].keys())
@@ -106,7 +93,7 @@ class BaseAgent:
         return entries
     
     # Level of abstraction of the in-context data should come in here
-    def data_driven_in_context_prompt(self, in_context_data):
+    def _in_context_prompt(self, in_context_data):
         """Create a system prompt containing in-context examples from similar episodes"""
         # Determine which set of entries to use
         success_entries, failure_entries = in_context_data[0], in_context_data[1]
@@ -116,7 +103,7 @@ class BaseAgent:
         if len(entries) == 0:
             return ""
         
-        entries = self.roll_up_trajectory(entries)
+        entries = self._roll_up_trajectory(entries)
         value_type = list(entries[0].keys()) # Keys from entries
 
         system_prompt = f"\nHere are some examples of {', '.join(value_type)} from episodes that {outcome} similar goals:\n"
@@ -132,26 +119,25 @@ class BaseAgent:
 
         return system_prompt
     
-    # Generic LLM function call augmented by input-output examples
-    # Could really use DSPy-type thing here for this
-    '''
-    async def execute_function(self, desc, keys, values):
-        """Execute a function with the given description, keys, and values"""
-        conversation = []
-        conversation.append({"role": "system", "content": desc})
-        if in_context_data:
-            system_prompt += self.data_driven_in_context_prompt(in_context_data)
-        conversation.append({"role": "user", "content": f"Keys: {keys}\nValues: {values}"})
-        response = await self.llm.generate_chat(conversation)
-        return response
-    '''
+    """ Main components used by agent's choose_action function """
+
+    def get_in_context_data(self, key_type, key, value_type, outcome="winning", k=5, window=20) -> List[Dict]:
+        """Retrieve in context examples from the database"""
+        success_entries, failure_entries = self.db.get_similar_entries(key_type, key, outcome=outcome, k=k, window=window)
+        # Now figure out which part of the examples to return in-context
+        if not isinstance(value_type, list): # Check that this is a list, not a string
+            value_type = [value_type]
+        # Filter out the keys that are not in the value_type
+        success_entries = [{k: v for k, v in entry.items() if k in value_type} for entry in success_entries]
+        failure_entries = [{k: v for k, v in entry.items() if k in value_type} for entry in failure_entries]
+        return success_entries, failure_entries
     
     async def create_plan(self, observation: Observation, available_actions: List[Action], in_context_data = None) -> str:
         """Generate a plan for the agent to follow"""
         conversation = []
         system_prompt = f"You are an expert at generating high-level plans of actions to achieve a goal. "
         if in_context_data:
-            system_prompt += self.data_driven_in_context_prompt(in_context_data)
+            system_prompt += self._in_context_prompt(in_context_data)
         conversation.append({"role": "system", "content": system_prompt})
         plan = await generate_plan(conversation, self.goal, self.llm)
         self.plan = plan
@@ -164,10 +150,10 @@ class BaseAgent:
         # Add system prompt
         system_prompt = f"You are an expert at reasoning about the most appropriate action to take towards achieving a goal. "
         if in_context_data:
-            system_prompt += self.data_driven_in_context_prompt(in_context_data)
+            system_prompt += self._in_context_prompt(in_context_data)
         conversation.append({"role": "system", "content": system_prompt})
         # Add conversation history
-        conversation.append({"role": "user", "content": self.create_conversation(["goal", "plan", "observation", "reasoning", "action"], available_actions) + "reasoning: "})
+        conversation.append({"role": "user", "content": self._create_conversation(["goal", "plan", "observation", "reasoning", "action"], available_actions) + "reasoning: "})
         reasoning = await self.llm.generate_chat(conversation)
         self.reasoning_history.append(reasoning)
         return reasoning
@@ -182,9 +168,9 @@ class BaseAgent:
         if "trad" in self.config.get("agent_type", "").lower():
             system_prompt += "\nHere is your action space:\n" + self.action_space['description']
         if in_context_data:
-            system_prompt += self.data_driven_in_context_prompt(in_context_data)
+            system_prompt += self._in_context_prompt(in_context_data)
         conversation.append({"role": "system", "content": system_prompt})
-        conversation.append({"role": "user", "content": self.create_conversation(["goal", "plan", "observation", "reasoning", "action"], available_actions) + "action: "})
+        conversation.append({"role": "user", "content": self._create_conversation(["goal", "plan", "observation", "reasoning", "action"], available_actions) + "action: "})
         
         # Select action
         action = await self.llm.generate_chat(conversation)
@@ -197,16 +183,18 @@ class BaseAgent:
 
         return action
     
+    """ Main components used by agent's process_feedback function """
+    
     async def reflect(self, observation: Observation, reward: float) -> List[Dict]:
         """Reflect on the conversation and observation"""
-        user_prompt = self.create_conversation(["goal", "plan", "observation", "reasoning", "action"], [])
+        user_prompt = self._create_conversation(["goal", "plan", "observation", "reasoning", "action"], [])
         response = await self.llm.generate_chat([{"role": "system", "content": f"You are an agent in an environment. Given the goal: {self.goal}, your task is to reflect on the trajectory of observations and actions taken. Identify any mistakes or areas for improvement in the plan or execution."}, {"role": "user", "content": user_prompt}]) 
         return response
     
     async def summarize(self, obs=None) -> str:
         """Summarize the conversation and observation"""
         conversation = []
-        conversation = self.create_conversation(conversation, None, None, None)
+        conversation = self._create_conversation(conversation, None, None, None)
         if obs is None:
             summary = await trajectory_summary(self.goal, conversation, self.llm)
         else:
@@ -216,6 +204,12 @@ class BaseAgent:
         summary = Observation(summary)
         return summary
     
+    def store_episode(self, reflection, summary):
+        """Store an episode in the database"""
+        self.db.store_episode(self.environment_id, self.goal, self.category,self.observation_history, self.reasoning_history, self.action_history, self.reward_history, self.plan, reflection, summary)
+
+    """ Placeholder functions for agent's choose_action and process_feedback functions """
+    
     async def choose_action(self, obs, valid_actions, log_file):
         """Choose an action from available actions given the current observation"""
         pass
@@ -223,6 +217,8 @@ class BaseAgent:
     async def process_feedback(self, new_obs, reward, done, log_file):
         """Process feedback from the environment"""
         pass
+
+    """ Between-episode functions for the outer loop """
         
     def clear_history(self):
         """Clear the agent's history"""
