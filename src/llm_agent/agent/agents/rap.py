806acfdb36ec8ce1c2add4ad8783d80db6337a66
VSAnimator <sarukkai@stanford.edu>
2025-01-25 17:03:44 -0800

    small rap syntax fix

--- src/llm_agent/agent/agents/rap.py
+++ src/llm_agent/agent/agents/rap.py
@@ -10,7 +10,7 @@ class RAP(BaseAgent):
             plan_data = self.get_trajectory_data(key_types=["goal", "category"], keys=[self.goal, self.category], value_types=["goal", "plan"], outcome="winning", k=3)
             await self.create_plan(obs, valid_actions, in_context_data=plan_data) 
         if self.in_context_data is None:
-            self.in_context_data = self.get_in_context_data(key_types=["goal", "category", "plan", "observation"], keys=[self.goal, self.category, self.plan, obs.structured], value_types=["goal", "observation", "reasoning", "action"], outcome="winning", k=4, window=5)
+            self.in_context_data = self.get_state_data(trajectory_key_types=["goal", "category", "plan"], trajectory_keys=[self.goal, self.category, self.plan], state_key_types=["observation"], state_keys=[obs.structured], value_types=["goal", "observation", "reasoning", "action"], outcome="winning", k=4, window=5)
         reasoning = await self.reason(obs, valid_actions, in_context_data=self.in_context_data)
         self.in_context_data = self.get_state_data(trajectory_key_types=["goal", "category", "plan"], trajectory_keys=[self.goal, self.category, self.plan], state_key_types=["reasoning"], state_keys=[reasoning], value_types=["goal", "plan", "observation", "reasoning", "action"], outcome="winning", k=4, window=5)
         action = await self.act(obs, valid_actions, reasoning, in_context_data=self.in_context_data) 
