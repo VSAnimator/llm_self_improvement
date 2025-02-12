19fd67eafd30de8672e806b85744326e2f0f658d
VSAnimator <sarukkai@stanford.edu>
2025-02-12 12:36:27 -0800

    Potential for multiline reasoning and action

--- src/llm_agent/agent/agents/autoguide.py
+++ src/llm_agent/agent/agents/autoguide.py
@@ -10,11 +10,6 @@ class AutoGuide(BaseAgent):
         """Choose an action from available actions given the current observation"""
         # Use the previos reflections as in-context data
         example_data = self.get_trajectory_data(key_types=["category"], keys=[self.category], value_types=["goal", "observation", "reasoning", "action"], outcome="winning", k=2) # TODO: revisit keys for this call...
-        '''
-        summary_data = self.get_rule_data(key_types=["observation"], keys=[obs.structured], value_types=["context", "observation","reasoning", "action"], outcome="winning", k=3) # Get a rule corresponding to a state similar to the current state
-        curr_context = self.summarize(obs, summary_data)
-        rule_data = self.get_rule_data(key_types=["context"], keys=[curr_context], value_types=["name", "context", "rule_content", "example_data"], outcome="winning", k=3)
-        '''
         rule_data = self.get_rule_data(trajectory_key_types=["category", "goal"], trajectory_keys=[self.category, self.goal], state_key_types=["observation"], state_keys=[obs.structured], value_types=["rules"], outcome=None, k=3, window=20) # Rules come from successes and failures # Should bring back the actual example data for positive rules, not sure what to do when fetching both positive and negative rules
         print("Example data: ", example_data)
         print("Rule data: ", rule_data)
@@ -28,12 +23,6 @@ class AutoGuide(BaseAgent):
     
     async def batch_analyze_episodes(self):
         """For AutoGuide, this is where we generate context-aware guidelines"""
-        # We loop through contrastive pairs of episodes, and generate rules for each pair
-        #contrastive_pairs = self.get_contrastive_pairs()
-        #for pair in contrastive_pairs:
-        #    self.generate_rule(pair) # This also takes into account existing rules in the database
-            # Find most similar rules in database (or this could be done internally in the consolidate call...)
-            #similar_rules = self.get_rule_data(key_types=["name"], keys=[rule['name']], value_types=["name", "context", "rule_content"], outcome="winning", k=2)
         await self.generate_rules(mode="pair", environment_id="all")
         await self.consolidate_rules() # Basically identifies shared contexts/names and consolidates the lists. Store under the same context/name if similar enough, and can modify existing names/contexts to match if needed. 
         pass
